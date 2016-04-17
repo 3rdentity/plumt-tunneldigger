43f654fc9cc950770e6d37bf5deae5900c539a86
Jaka <build@rubo.breskev.com>
2016-04-17 22:51:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# plumt-tunneldigger
+================
+
+This is a Debian package for wlan Slovenija tunneldigger client. Tested under Debian Jessie.
+
+----------------
+
+Build package in three steps:
+
+`git clone https://github.com/plumt/plumt-tunneldigger.git`
+
+`cd plumt-tunneldigger/`
+
+`fakeroot debian/rules binary`
---
+++ configure.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DIR="$1"
+[ -n "$DIR" ] || exit 1
+
+DEST="${DIR%/}/source"
+
+git clone https://github.com/wlanslovenija/tunneldigger.git "$DEST"
+
+cp "${DIR%/}/build/Makefile" "$DEST/client/Makefile"
+make -C "$DEST/client/"
+
+mkdir -p "${DIR%/}/usr/sbin/"
+mv "$DEST/client/l2tp_client" "${DIR%/}/usr/sbin/tunneldigger"
+
+rm -fr "$DEST"
